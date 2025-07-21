e71de593f69f0d72e27813533745c788e9a18547
kowalsky011 <kowalsky01@list.ru>
2025-07-21 14:28:29 +0400

    Добавил чота

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Сосал? Ебали!");
+
+        System.out.println("Проебал? Обосался!");
 
     }
 }
